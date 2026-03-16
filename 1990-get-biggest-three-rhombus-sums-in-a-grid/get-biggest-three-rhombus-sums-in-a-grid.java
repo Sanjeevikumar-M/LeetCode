164f6f9b946338441439414b0586c2beedc69aef
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-03-16 10:18:48 +0530

    Time: 15 ms (100.00%) | Memory: 46.5 MB (100.00%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -1,50 +1,52 @@
 class Solution {
+    private int first = -1;
+    private int second = -1;
+    private int third = -1;
     public int[] getBiggestThree(int[][] grid) {
-        int m = grid.length,n=grid[0].length;
-
-        int[][] diag1 = new int[m+1][n+1];
-        int[][] diag2 = new int[m+1][n+2];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                diag1[i+1][j+1] = grid[i][j] + diag1[i][j];
-                diag2[i+1][j] = grid[i][j] + diag2[i][j+1];
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                add(grid[i][j]);
             }
         }
-
-        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
-
-        for(int r=0;r<m;r++){
-            for(int c=0;c<n;c++){
-                set.add(grid[r][c]);
-                for(int k=1; ;k++){
-                    if(r-k<0 || r+k>=m ||c-k<0 ||c+k>=n)
-                       break;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 1;
+                     i - k >= 0 && i + k < m && j - k >= 0 && j + k < n;
+                     k++) {
                     int sum = 0;
+                    for (int d = 0; d < k; d++)
+                        sum += grid[i - k + d][j + d];
+                    for (int d = 0; d < k; d++)
+                        sum += grid[i + d][j + k - d];
+                    for (int d = 0; d < k; d++)
+                        sum += grid[i + k - d][j - d];
+                    for (int d = 0; d < k; d++)
+                        sum += grid[i - d][j - k + d];
 
-                    sum+=diag1[r+1][c+k+1] - diag1[r-k][c];
-                    sum+=diag2[r+1][c-k] - diag2[r-k][c+1];
-                    sum+=diag1[r+k+1][c+1] - diag1[r][c-k];
-                    sum+=diag2[r+k+1][c] - diag2[r][c+k+1];
-
-                    sum-=grid[r-k][c];
-                    sum-=grid[r][c+k];
-                    sum-=grid[r+k][c];
-                    sum-=grid[r][c-k];
-
-                    set.add(sum);
+                    add(sum);
                 }
             }
         }
-        int size = Math.min(3,set.size());
-        int[] res = new int[size];
-        int i=0;
-
-        for(int val:set){
-            if(i==size) break;
-            res[i++]=val;
+        if (second == -1) return new int[]{first};
+        if (third == -1) return new int[]{first, second};
+        return new int[]{first, second, third};
+    }
+    private void add(int val) {
+        if (val == first || val == second || val == third) return;
+        if (val > first) {
+            third = second;
+            second = first;
+            first = val;
+        } 
+        else if (val > second) 
+        {
+            third = second;
+            second = val;
+        } 
+        else if (val > third) 
+        {
+            third = val;
         }
-
-        return res;
     }
 }
