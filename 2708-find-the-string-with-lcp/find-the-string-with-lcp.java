8cc8bdb4ee8d6e8580edc1bdd45bdba64475e631
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-03-28 21:05:40 +0530

    Time: 5 ms (100.00%) | Memory: 173.3 MB (80.00%) - LeetSync

--- 2708-find-the-string-with-lcp/find-the-string-with-lcp.java
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.java
@@ -1,73 +1,47 @@
 class Solution {
+
     public String findTheString(int[][] lcp) {
         int n = lcp.length;
+        char[] word = new char[n];
+        char current = 'a';
 
-        // Step 1: Validate diagonal
+        // construct the string starting from 'a' to 'z' sequentially
         for (int i = 0; i < n; i++) {
-            if (lcp[i][i] != n - i) return "";
-        }
-
-        // Step 2: DSU (Union-Find)
-        int[] parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        // Find
-        java.util.function.IntUnaryOperator find = new java.util.function.IntUnaryOperator() {
-            public int applyAsInt(int x) {
-                if (parent[x] != x)
-                    parent[x] = applyAsInt(parent[x]);
-                return parent[x];
-            }
-        };
-
-        // Union
-        java.util.function.BiConsumer<Integer, Integer> union = (a, b) -> {
-            int pa = find.applyAsInt(a);
-            int pb = find.applyAsInt(b);
-            if (pa != pb) parent[pa] = pb;
-        };
-
-        // Step 3: Merge groups
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (lcp[i][j] > 0) {
-                    union.accept(i, j);
+            if (word[i] == 0) {
+                if (current > 'z') {
+                    return "";
                 }
+                word[i] = current;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = word[i];
+                    }
+                }
+                current++;
             }
         }
 
-        // Step 4: Assign characters
-        char[] res = new char[n];
-        java.util.Map<Integer, Character> map = new java.util.HashMap<>();
-        char ch = 'a';
-
-        for (int i = 0; i < n; i++) {
-            int p = find.applyAsInt(i);
-            if (!map.containsKey(p)) {
-                if (ch > 'z') return ""; // more than 26 groups
-                map.put(p, ch++);
-            }
-            res[i] = map.get(p);
-        }
-
-        // Step 5: Validate by recomputing LCP
-        int[][] calc = new int[n][n];
-
+        // verify if the constructed string meets the LCP matrix requirements
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (res[i] == res[j]) {
-                    if (i + 1 < n && j + 1 < n)
-                        calc[i][j] = 1 + calc[i + 1][j + 1];
-                    else
-                        calc[i][j] = 1;
+                if (word[i] != word[j]) {
+                    if (lcp[i][j] != 0) {
+                        return "";
+                    }
                 } else {
-                    calc[i][j] = 0;
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) {
+                            return "";
+                        }
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
+                            return "";
+                        }
+                    }
                 }
-
-                if (calc[i][j] != lcp[i][j]) return "";
             }
         }
 
-        return new String(res);
+        return new String(word);
     }
 }
