448c3416ec763fd07e15da85a43f2dece8f1ff5e
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-03-30 18:21:09 +0530

    Time: 3 ms (100.00%) | Memory: 48.1 MB (53.95%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,29 +1,28 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int n = s1.length();
-        
-        int[] even1 = new int[26];
-        int[] odd1 = new int[26];
-        int[] even2 = new int[26];
-        int[] odd2 = new int[26];
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                even1[s1.charAt(i) - 'a']++;
-                even2[s2.charAt(i) - 'a']++;
-            } else {
-                odd1[s1.charAt(i) - 'a']++;
-                odd2[s2.charAt(i) - 'a']++;
-            }
+        int  n = s1.length();
+        int freq1[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
         }
-        
-        // Compare frequency arrays
-        for (int i = 0; i < 26; i++) {
-            if (even1[i] != even2[i] || odd1[i] != odd2[i]) {
-                return false;
-            }
+        int freq2[]=new int[26];
+        for(int i=0;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
         }
-        
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+            freq1[i]=0;
+            freq2[i]=0;
+        }
+        for(int i=1;i<n;i+=2){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        for(int i=1;i<n;i+=2){
+            freq2[s2.charAt(i)-'a']++;
+        }
+        for(int i =0;i<26;i++){
+            if(freq1[i]!=freq2[i])return false;
+        }
         return true;
     }
 }
