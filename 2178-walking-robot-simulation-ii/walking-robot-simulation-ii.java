11d9cfe9ea1802b8ff482401355c0b34de700b3a
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-04-07 19:18:51 +0530

    Time: 57 ms (99.30%) | Memory: 56 MB (99.30%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -1,59 +1,101 @@
 class Robot {
 
+    int direction;
+    String[] directionName;
     int width, height;
-    int perimeter;
-    int pos;
-    boolean moved;
+    int[] position;
+    int[][] stepMovement;
+    int num;
 
     public Robot(int width, int height) {
         this.width = width;
         this.height = height;
-        this.perimeter = 2 * (width + height) - 4;
-        this.pos = 0;
-        this.moved = false;
-    }
+        this.position = new int[2];
+        this.position[0] = 0;
+        this.position[1] = 0;
+        this.direction = 0;
+        this.directionName = new String[]{"East", "North", "West", "South"};
+        this.stepMovement = new int[][]{{1,0},  {0,1}, {-1, 0}, {0, -1}};
+        this.num = 0;
 
+    }
+    
+    private void lazyStep(int num){
+        num = num%(2*(this.width + this.height) - 4);
+        if(num == 0){
+            num = 2*(this.width + this.height) - 4;
+        }
+        while(num > 0){
+            switch(direction){
+                case 0:
+                    if(this.position[0] + num > this.width - 1){
+                        num = num - (this.width - this.position[0] - 1);
+                        this.direction = (this.direction + 1)%4;
+                        this.position[0] = this.width-1;
+                    }
+                    else{
+                        this.position[0] = this.position[0] + num;
+                        num = 0;
+                    }
+                    break;
+                case 2:
+                    if(this.position[0] - num < 0){
+                        num = num - (this.position[0]);
+                        this.direction = (this.direction + 1)%4;
+                        this.position[0] = 0;
+                    }
+                    else{
+                        this.position[0] = this.position[0] - num;
+                        num = 0;
+                    }
+                    break;
+                case 1:
+                    if(this.position[1] + num > this.height - 1){
+                        num = num - (this.height - this.position[1] - 1);
+                        this.direction = (this.direction + 1)%4;
+                        this.position[1] = this.height - 1;
+                    }
+                    else{
+                        this.position[1] = this.position[1] + num;
+                        num = 0;
+                    }
+                    break;
+                case 3:
+                    if(this.position[1] - num < 0){
+                        num = num - (this.position[1]);
+                        this.direction = (this.direction + 1)%4;
+                        this.position[1] = 0;
+                    }
+                    else{
+                        this.position[1] = this.position[1] - num;
+                        num = 0;
+                    }
+                    break;
+            }
+        }
+        this.num = 0;
+    }
     public void step(int num) {
-
-        int move = num % perimeter;
-
-        if (num > 0) moved = true;
-
-        pos = (pos + move) % perimeter;
+        this.num += num;
     }
-
+    
     public int[] getPos() {
-        int x, y;
-
-        if (pos < width) {
-            x = pos;
-            y = 0;
-        } 
-        else if (pos < width + height - 1) {
-            x = width - 1;
-            y = pos - (width - 1);
-        } 
-        else if (pos < 2 * width + height - 2) {
-            x = (width - 1) - (pos - (width + height - 2));
-            y = height - 1;
-        } 
-        else {
-            x = 0;
-            y = (height - 1) - (pos - (2 * width + height - 3));
-        }
-
-        return new int[]{x, y};
+        if(this.num > 0)
+            this.lazyStep(this.num);
+        return this.position;
     }
-
+    
     public String getDir() {
-
-        if (pos == 0) {
-            return moved ? "South" : "East";
-        }
-
-        if (pos < width) return "East";
-        else if (pos < width + height - 1) return "North";
-        else if (pos < 2 * width + height - 2) return "West";
-        else return "South";
+        if(this.num > 0)
+            this.lazyStep(this.num);
+        return this.directionName[this.direction];
     }
 }
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */
