b341c2bc7acc4fc9362669915d01935bd28a3ae4
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-04-23 18:24:48 +0530

    Time: 14 ms (100.00%) | Memory: 107.8 MB (95.07%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -1,46 +1,28 @@
-import java.util.*;
-
 class Solution {
+    private class Data {
+        long s1, s2;
+        int c1, c2;
+    }
+
     public long[] distance(int[] nums) {
         int n = nums.length;
-        long[] res = new long[n];
-
-        // Step 1: group indices
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, Data> m = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            Data d = m.get(nums[i]);
+            if (d == null)
+                m.put(nums[i], d = new Data());
+            d.s2 += i;
+            d.c2 += 1;
         }
-
-        // Step 2: process each group
-        for (List<Integer> list : map.values()) {
-            int size = list.size();
-
-            // prefix sum of indices
-            long[] prefix = new long[size];
-            prefix[0] = list.get(0);
-            for (int i = 1; i < size; i++) {
-                prefix[i] = prefix[i - 1] + list.get(i);
-            }
-
-            for (int i = 0; i < size; i++) {
-                long idx = list.get(i);
-
-                // left
-                long left = 0;
-                if (i > 0) {
-                    left = i * idx - prefix[i - 1];
-                }
-
-                // right
-                long right = 0;
-                if (i < size - 1) {
-                    right = (prefix[size - 1] - prefix[i]) - (size - i - 1) * idx;
-                }
-
-                res[list.get(i)] = left + right;
-            }
+        long[] r = new long[n];
+        for (int i = 0; i < n; i++) {
+            Data x = m.get(nums[i]);
+            r[i] = x.s2 - x.s1 + 1L * x.c1 * i - 1L * i * x.c2;
+            x.s1 += i;
+            x.s2 -= i;
+            x.c1 += 1;
+            x.c2 -= 1;
         }
-
-        return res;
+        return r;
     }
 }
