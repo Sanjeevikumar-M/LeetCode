9bc5cb822334359a737c0d40d20954a813777ea1
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-04-28 12:09:51 +0530

    Time: 9 ms (96.18%) | Memory: 107.9 MB (78.63%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,40 +1,32 @@
-import java.util.*;
-
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int m = grid.length, n = grid[0].length;
-        int size = m * n;
-        
-        int[] arr = new int[size];
-        int index = 0;
-        
-        // Flatten the grid
+        int n = grid.length, m = grid[0].length;
+        int[] freq = new int[10001];
+        //check -1 cases
+        boolean foundFalse = false;
+        int currChoice = grid[0][0];
         for (int[] row : grid) {
-            for (int num : row) {
-                arr[index++] = num;
+            for (int y : row) {
+                freq[y]++;
+                if (Math.abs(currChoice - y) % x != 0)
+                    foundFalse = true;
             }
         }
-        
-        // Check feasibility
-        int remainder = arr[0] % x;
-        for (int num : arr) {
-            if (num % x != remainder) {
-                return -1;
+        if (foundFalse) return -1;
+        //sorting
+        int[] arr = new int[n * m];
+        int k = 0;
+        for (int i = 0; i < 10001; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                arr[k++] = i;
             }
         }
-        
-        // Sort array
-        Arrays.sort(arr);
-        
-        // Median
-        int median = arr[size / 2];
-        
-        // Calculate operations
-        int operations = 0;
+        int count = 0;
+        //pick best middle element
+        int median = arr[arr.length / 2];
         for (int num : arr) {
-            operations += Math.abs(num - median) / x;
+            count += Math.abs(num - median) / x;
         }
-        
-        return operations;
+        return count;
     }
 }
