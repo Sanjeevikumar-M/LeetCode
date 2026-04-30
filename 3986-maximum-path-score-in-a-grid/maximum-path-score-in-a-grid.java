b066bf4a9b9a978210ff703d009240a85630c0ef
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-04-30 19:43:07 +0530

    Time: 258 ms (80.65%) | Memory: 86.5 MB (48.93%) - LeetSync

---
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maxPathScore(int[][] grid, int k) {
+        int m = grid.length, n = grid[0].length;
+
+        int[][][] dp = new int[m][n][k + 1];
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                for (int c = 0; c <= k; c++)
+                    dp[i][j][c] = -1;
+
+        dp[0][0][0] = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int c = 0; c <= k; c++) {
+                    if (dp[i][j][c] == -1) continue;
+
+                    // down
+                    if (i + 1 < m) {
+                        int val = grid[i + 1][j];
+                        int cost = (val == 0) ? 0 : 1;
+                        if (c + cost <= k) {
+                            dp[i + 1][j][c + cost] =
+                                Math.max(dp[i + 1][j][c + cost], dp[i][j][c] + val);
+                        }
+                    }
+
+                    // right
+                    if (j + 1 < n) {
+                        int val = grid[i][j + 1];
+                        int cost = (val == 0) ? 0 : 1;
+                        if (c + cost <= k) {
+                            dp[i][j + 1][c + cost] =
+                                Math.max(dp[i][j + 1][c + cost], dp[i][j][c] + val);
+                        }
+                    }
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int c = 0; c <= k; c++) {
+            ans = Math.max(ans, dp[m - 1][n - 1][c]);
+        }
+
+        return ans;
+    }
+}
