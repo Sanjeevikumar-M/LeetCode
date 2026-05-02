e3559304bde9056a151323bf21cde1bb75ae26d4
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-05-02 16:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (82.58%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,35 +1,39 @@
+
 class Solution {
     public int rotatedDigits(int n) {
-        int count = 0;
-
-        for (int i = 1; i <= n; i++) {
-            if (isGood(i)) {
-                count++;
-            }
-        }
-
-        return count;
+        char[] digits = String.valueOf(n).toCharArray();
+        Integer[][][] memo = new Integer[digits.length][2][2];
+        return dfs(0, 1, 0, digits, memo);
     }
 
-    private boolean isGood(int num) {
-        boolean changed = false;
-
-        while (num > 0) {
-            int digit = num % 10;
-
-            // Invalid digits
-            if (digit == 3 || digit == 4 || digit == 7) {
-                return false;
-            }
-
-            // Digits that change
-            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
-                changed = true;
-            }
-
-            num /= 10;
+    private int dfs(int pos, int tight, int changed, char[] digits, Integer[][][] memo) {
+        if (pos == digits.length) {
+            return changed == 1 ? 1 : 0;
         }
 
-        return changed;
+        if (memo[pos][tight][changed] != null) {
+            return memo[pos][tight][changed];
+        }
+
+        int limit = tight == 1 ? digits[pos] - '0' : 9;
+        int count = 0;
+
+        for (int d = 0; d <= limit; d++) {
+            if (d == 3 || d == 4 || d == 7) {
+                continue;
+            }
+
+            int nextTight = tight == 1 && d == limit ? 1 : 0;
+            int nextChanged = changed;
+
+            if (d == 2 || d == 5 || d == 6 || d == 9) {
+                nextChanged = 1;
+            }
+
+            count += dfs(pos + 1, nextTight, nextChanged, digits, memo);
+        }
+
+        memo[pos][tight][changed] = count;
+        return count;
     }
 }
