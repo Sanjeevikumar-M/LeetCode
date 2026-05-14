f1d2c506f5bd90362a73b2f5b6cc91bac1eee1ff
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-05-14 14:53:12 +0530

    Time: 1 ms (91.05%) | Memory: 44.4 MB (91.62%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,34 +1,26 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int n = nums.length;
 
-        int max = 0;
-        for (int x : nums) {
-            max = Math.max(max, x);
+        int n = 0;
+
+        // find max element
+        for (int num : nums) {
+            n = Math.max(n, num);
         }
 
-        // Length must be max + 1
-        if (max != n - 1) {
-            return false;
+        // size check
+        if (nums.length != n + 1) return false;
+
+        int[] freq = new int[201];
+
+        for (int num : nums) {
+            freq[num]++;
         }
 
-        int[] freq = new int[max + 1];
-
-        for (int x : nums) {
-            if (x > max) {
-                return false;
-            }
-            freq[x]++;
+        for (int i = 1; i < n; i++) {
+            if (freq[i] != 1) return false;
         }
 
-        // 1 to max-1 should appear once
-        for (int i = 1; i < max; i++) {
-            if (freq[i] != 1) {
-                return false;
-            }
-        }
-
-        // max should appear twice
-        return freq[max] == 2;
+        return freq[n] == 2;
     }
 }
