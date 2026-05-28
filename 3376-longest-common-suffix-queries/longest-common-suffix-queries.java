a34daeb7058744b789559ba719ad8d7c12c7c94f
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-05-28 17:27:48 +0530

    Time: 47 ms (88.95%) | Memory: 284.7 MB (11.05%) - LeetSync

--- 3376-longest-common-suffix-queries/longest-common-suffix-queries.java
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.java
@@ -1,42 +1,65 @@
+// import java.util.*;
+// class Solution {
+//     public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
+//         HashMap<String, int[]> map = new HashMap<>();
+//         int[] result = new int[wordsQuery.length];
+//         for(int i = 0; i < wordsContainer.length; ++i){
+//             int cur_len = wordsContainer[i].length();
+//             for(int j = cur_len; j >= 0; --j){
+//                 String s = wordsContainer[i].substring(j,cur_len);
+//                 if(!map.containsKey(s)){
+//                     map.put(s, new int[]{cur_len, i});
+//                 }
+//                 else{
+//                     if(map.get(s)[0] > cur_len){
+//                         map.get(s)[0] = cur_len;
+//                         map.get(s)[1] = i;
+//                     }
+//                 }
+//             }
+//         } 
+//         for(int i = 0; i < wordsQuery.length; ++i){  
+//             int cur_len = wordsQuery[i].length();
+//             for(int j = cur_len; j >= 0; --j){
+//                 String s = wordsQuery[i].substring(j,cur_len);
+//                 if(map.containsKey(s)){
+//                     result[i] = map.get(s)[1];
+//                 }
+//                 else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
 
-    static class TrieNode {
+    class TrieNode {
         TrieNode[] child = new TrieNode[26];
 
-        // Best index for this node
-        int idx = -1;
+        int len = Integer.MAX_VALUE;
+        int idx = 0;
     }
 
     TrieNode root = new TrieNode();
-    String[] words;
 
-    public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
-        words = wordsContainer;
+    void insert(String s, int index) {
 
-        // Build Trie using reversed words
-        for (int i = 0; i < wordsContainer.length; i++) {
-            insert(wordsContainer[i], i);
-        }
-
-        int[] ans = new int[wordsQuery.length];
-
-        for (int i = 0; i < wordsQuery.length; i++) {
-            ans[i] = query(wordsQuery[i]);
-        }
-
-        return ans;
-    }
-
-    private void insert(String word, int index) {
         TrieNode node = root;
 
-        // Update root best index
-        if (node.idx == -1 || better(index, node.idx)) {
+        int n = s.length();
+
+        // update root
+        if (n < node.len) {
+            node.len = n;
             node.idx = index;
         }
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            int c = word.charAt(i) - 'a';
+        for (int i = n - 1; i >= 0; --i) {
+
+            int c = s.charAt(i) - 'a';
 
             if (node.child[c] == null) {
                 node.child[c] = new TrieNode();
@@ -44,35 +67,47 @@ class Solution {
 
             node = node.child[c];
 
-            // Store best index at each node
-            if (node.idx == -1 || better(index, node.idx)) {
+            if (n < node.len) {
+                node.len = n;
                 node.idx = index;
             }
         }
     }
 
-    private int query(String word) {
+    int search(String s) {
+
         TrieNode node = root;
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            int c = word.charAt(i) - 'a';
+        int ans = node.idx;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+
+            int c = s.charAt(i) - 'a';
 
             if (node.child[c] == null) {
                 break;
             }
 
             node = node.child[c];
+
+            ans = node.idx;
         }
 
-        return node.idx;
+        return ans;
     }
 
-    // Return true if a is better than b
-    private boolean better(int a, int b) {
-        if (words[a].length() != words[b].length()) {
-            return words[a].length() < words[b].length();
+    public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
+
+        for (int i = 0; i < wordsContainer.length; ++i) {
+            insert(wordsContainer[i], i);
         }
 
-        return a < b;
+        int[] res = new int[wordsQuery.length];
+
+        for (int i = 0; i < wordsQuery.length; ++i) {
+            res[i] = search(wordsQuery[i]);
+        }
+
+        return res;
     }
 }
