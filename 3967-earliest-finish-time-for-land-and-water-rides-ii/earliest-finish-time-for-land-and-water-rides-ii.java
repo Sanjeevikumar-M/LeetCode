571103d49952e30f1571d8a2a9bceae67a19fcd5
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-03 18:56:08 +0530

    Time: 3 ms (50.00%) | Memory: 91.9 MB (74.14%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -4,18 +4,19 @@ class Solution {
         int m = waterDuration.length;
         int a1 = Integer.MAX_VALUE;
         int a2 = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            a1 = Math.min(a1, landStartTime[i] + landDuration[i]);
+
+        for(int i=0;i<n;i++){
+            a1 = Math.min(a1,landStartTime[i]+landDuration[i]);
         }
-        for (int i = 0; i < m; i++) {
-            a2 = Math.min(a2, waterStartTime[i] + waterDuration[i]);
+        for(int i=0;i<m;i++){
+            a2 = Math.min(a2,waterStartTime[i]+waterDuration[i]);
         }
         int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            ans = Math.min(ans, Math.max(a2, landStartTime[i]) + landDuration[i]);
+        for(int i=0;i<n;i++){
+            ans = Math.min(ans,Math.max(a2,landStartTime[i])+landDuration[i]);
         }
-        for (int i = 0; i < m; i++) {
-            ans = Math.min(ans, Math.max(a1, waterStartTime[i]) + waterDuration[i]);
+        for(int i=0;i<m;i++){
+            ans = Math.min(ans,Math.max(a1,waterStartTime[i])+waterDuration[i]);
         }
         return ans;
     }
