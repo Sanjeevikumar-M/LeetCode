70991dfd9f83a3e54788ec27ce2bb2430a3aa505
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-05 18:58:02 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (91.94%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -3,14 +3,10 @@ class Solution:
         left = 0
         right = len(arr)-1
         while left<=right:
-            if arr[left]==arr[right] and left==right:
-                return left
-
-            elif arr[left]<=arr[right]:
-                left += 1
-
+            mid = left + (right-left)//2
+            if arr[mid]<=arr[mid+1]:
+                left = mid + 1
             else:
-                right -= 1
-
-        return -1
+                right = mid - 1 
+        return left
 
