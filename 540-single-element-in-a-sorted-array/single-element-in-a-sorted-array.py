5fc306c6cb34890d52d172d3d7f22694f8d84ee9
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-05 19:10:59 +0530

    Time: 7 ms (8.42%) | Memory: 29.3 MB (5.09%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        map = {}
+        map={}
         for i in nums:
-            map[i] = map.get(i,0)+1
+            map[i]=map.get(i,0)+1
         for i in map:
             if map[i]==1:
                 return i
