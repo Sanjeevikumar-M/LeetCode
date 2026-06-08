2d8929729223b946497360a51404687a7c3ba896
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-08 09:16:01 +0530

    Time: 22 ms (86.14%) | Memory: 33.8 MB (55.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,21 +1,15 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        n = len(nums)
-        mid = n//2
+        x=[]
+        y=[]
+        z=[]
+        for i in nums:
+            if i<pivot:
+                x.append(i)
+            elif i==pivot:
+                y.append(i)
+            else:
+                z.append(i)
+        return x+y+z
 
-        if pivot not in nums:
-            return -1
-        else:
-            arrless = []
-            arrmid = []
-            arrgreater = []
-            for i in range(0,n):
-                if nums[i]<pivot:
-                    arrless.append(nums[i])
-                elif nums[i]>pivot:
-                    arrgreater.append(nums[i])
-                else:
-                    arrmid.append(nums[i])
-
-            arr = arrless + arrmid + arrgreater
-            return arr
+        
