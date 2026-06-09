90cd6e74df729616ad7d669275ea49241a46a2c4
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-09 10:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (8.55%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,15 +1,32 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        topthree = set()
-        for i in nums:
-            topthree.add(i)
-            if len(topthree)>3:
-                mi = min(topthree)
-                topthree.remove(mi)
-        if len(topthree)<3:
-            return max(topthree)
-        else:
-            return min(topthree)
-                
-            
-        
+        ################# Using 3 pointers ##############################
+        first_max = float('-inf')
+        second_max = float('-inf')
+        third_max = float('-inf')
+
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                continue
+            seen.add(num)
+            if num > first_max:
+                third_max, second_max, first_max = second_max, first_max, num
+            elif num > second_max:
+                third_max, second_max = second_max, num
+            elif num > third_max:
+                third_max = num
+
+        if third_max == float('-inf'):
+            return first_max
+        return third_max
+
+
+
+        ################ Using Sort #####################################
+        # sorted_list = list(sorted(set(nums))[::-1])
+        # if len(sorted_list) < 3:
+        #     return sorted_list[0]
+        # else:
+        #     return sorted_list[2]
