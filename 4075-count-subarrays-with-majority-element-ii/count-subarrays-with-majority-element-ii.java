78410ba3f80a4762ebd0db9772a24b918ca0cd5b
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-06-26 09:46:44 +0530

    Time: 2 ms (100.00%) | Memory: 87.9 MB (81.97%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -1,52 +1,24 @@
 class Solution {
-    // Fenwick Tree (Binary Indexed Tree) implementation
-    private void update(int[] bit, int index, int delta) {
-        while (index < bit.length) {
-            bit[index] += delta;
-            index += index & (-index);
-        }
-    }
-
-    private int query(int[] bit, int index) {
-        int sum = 0;
-        while (index > 0) {
-            sum += bit[index];
-            index -= index & (-index);
-        }
-        return sum;
-    }
 
     public long countMajoritySubarrays(int[] nums, int target) {
         int n = nums.length;
-        
-        // The prefix sum can range from -n to n. 
-        // We shift everything by + (n + 1) to keep indices positive for the BIT.
-        int offset = n + 1;
-        int maxVal = 2 * n + 2;
-        int[] bit = new int[maxVal];
-        
-        long totalSubarrays = 0;
-        int currentPrefSum = 0;
-        
-        // Insert the initial prefix sum (pref[0] = 0) into the BIT
-        update(bit, 0 + offset, 1);
-        
-        for (int num : nums) {
-            // Step 1: Update current prefix sum (+1 if match, -1 if mismatch)
-            if (num == target) {
-                currentPrefSum += 1;
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
+        long ans = 0;
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
             } else {
-                currentPrefSum -= 1;
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
             }
-            
-            // Step 2: Query the BIT to count how many previous prefix sums are strictly smaller
-            // currentPrefSum + offset - 1 gives us the upper bound for strictly smaller elements
-            totalSubarrays += query(bit, currentPrefSum + offset - 1);
-            
-            // Step 3: Add the current prefix sum to the BIT
-            update(bit, currentPrefSum + offset, 1);
+            ans += presum;
         }
-        
-        return totalSubarrays;
+        return ans;
     }
 }
