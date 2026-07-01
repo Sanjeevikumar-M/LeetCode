e193a6492c2f1e26aded9456784170326a7d2dd4
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-01 22:48:37 +0530

    Time: 17 ms (57.87%) | Memory: 52.9 MB (84.77%) - LeetSync

--- 1543-simplified-fractions/simplified-fractions.java
+++ 1543-simplified-fractions/simplified-fractions.java
@@ -1,17 +1,19 @@
 class Solution {
     public List<String> simplifiedFractions(int n) {
         List<String> result = new ArrayList<>();
-        HashSet<Float> set = new HashSet<>();
 
-        for(int i=1;i<n;i++){
-            for(int j=i+1;j<=n;j++){
-                float f = (float) i/j;
-                if(!set.contains(f)){
-                    set.add(f);
-                    result.add((i+"/"+j));
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(gcd(j,i)==1){
+                    result.add(j+"/"+i);
                 }
             }
         }
         return result;
     }
+
+    private static int gcd(int a,int b){
+        return b==0 ? a : gcd(b,(a%b));
+    }
+
 }
