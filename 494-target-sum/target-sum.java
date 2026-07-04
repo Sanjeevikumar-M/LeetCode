3bcedbab939cc8a22f1d9b0c76a8dcf61f2c76e0
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-04 11:29:19 +0530

    Time: 2 ms (99.93%) | Memory: 42.8 MB (93.94%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -4,15 +4,14 @@ class Solution {
         for(int num:nums){
             totalSum += num;
         }
-        if(target+totalSum < 0 || (totalSum+target)%2!=0){
+        if(totalSum+target < 0 || (totalSum+target)%2 !=0){
             return 0;
         }
-
         int s1 = (target+totalSum)/2;
         int[] dp = new int[s1+1];
         dp[0] = 1;
         for(int num:nums){
-            for(int i=s1;i>=num;i--){
+            for(int i=s1;i>num-1;i--){
                 dp[i] += dp[i-num];
             }
         }
