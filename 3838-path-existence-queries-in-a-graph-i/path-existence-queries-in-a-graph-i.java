2c16dec3613b92df70d74d1fc7bb29efd7fcb0f4
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-09 10:15:07 +0530

    Time: 3 ms (97.03%) | Memory: 163.2 MB (59.41%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -13,9 +13,7 @@ class Solution {
 
         boolean[] answer = new boolean[queries.length];
         for(int i=0;i<queries.length;i++){
-            int u = queries[i][0];
-            int v = queries[i][1];
-            answer[i] = (comp[u]==comp[v]);
+            answer[i] = (comp[queries[i][0]]==comp[queries[i][1]]);
         }
         return answer;
     }
