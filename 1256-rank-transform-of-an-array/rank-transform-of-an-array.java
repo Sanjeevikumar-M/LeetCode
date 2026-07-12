7a18cf06978ec952bb8206625a951f8c499a4c9b
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-12 17:50:02 +0530

    Time: 28 ms (93.14%) | Memory: 77.1 MB (25.63%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,28 +1,20 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        // 1. Clone the original array to preserve the order, then sort the clone
-        int[] sortedArr = arr.clone();
-        Arrays.sort(sortedArr);
-        
-        // 2. Map each unique element to its rank starting from 1
-        HashMap<Integer, Integer> rankMap = new HashMap<>();
-        int rank = 1;
-        for (int num : sortedArr) {
-            // Only assign a rank if the element hasn't been ranked yet (handles duplicates)
-            if (!rankMap.containsKey(num)) {
-                rankMap.put(num, rank);
-                rank++;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] nums=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            nums[i]=arr[i];
+        }
+        Arrays.sort(nums);
+        int ind=1;
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],ind++);
             }
         }
-        
-        // 3. Replace each element in the original array with its rank
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rankMap.get(arr[i]);
+        for(int i=0;i<arr.length;i++){
+            nums[i]=map.get(arr[i]);
         }
-        
-        return arr;
+        return nums;
     }
 }
