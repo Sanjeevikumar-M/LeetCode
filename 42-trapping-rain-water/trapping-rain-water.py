58e6d844198bfb9f1e7ecc1f04b1c30212a735a2
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-21 17:27:05 +0530

    Time: 10 ms (48.26%) | Memory: 20.9 MB (83.97%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -6,14 +6,14 @@ class Solution:
         rightMax = 0
         res = 0
         while left<right:
-            if height[left]<=height[right]:
-                if height[left]>leftMax:
+            if height[left]<height[right]:
+                if height[left]>=leftMax:
                     leftMax = height[left]
                 else:
                     res += leftMax-height[left]
                 left+=1
             else:
-                if height[right]>rightMax:
+                if height[right]>=rightMax:
                     rightMax = height[right]
                 else:
                     res += rightMax - height[right]
