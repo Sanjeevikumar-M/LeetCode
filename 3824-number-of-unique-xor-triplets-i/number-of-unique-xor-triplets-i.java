f6f6167793519e7a3fd50d72d806a82ad158c599
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-23 09:27:54 +0530

    Time: 1 ms (51.02%) | Memory: 125.5 MB (26.53%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -1,10 +1,14 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
+        // int n = nums.length;
+
+        // if(n==1) return 1;
+        // if(n==2) return 2;
+
+        // return Integer.highestOneBit(n)*2;
         int n = nums.length;
-
-        if(n==1) return 1;
-        if(n==2) return 2;
-
-        return Integer.highestOneBit(n)*2;
+        if(n < 3) return n;
+        int nearest = (int)(Math.log(n)/Math.log(2));
+        return 1 << (nearest + 1);
     }
 }
