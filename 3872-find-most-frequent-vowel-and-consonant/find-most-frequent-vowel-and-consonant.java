a179e1ca6e5ca9dcc12c87d11c35b5451ab6bbfe
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-23 15:21:09 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (54.11%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxFreqSum(String s) {
-        int[] fq = new int[26];
-        for(char c:s.toCharArray()){
-            fq[c-'a']++;
+        int[] fr = new int[26];
+        for(char i:s.toCharArray()){
+            fr[i-'a']++;
         }
-        int maxv = 0, maxc = 0;
-        for(int i=0; i<26; i++){
-            int f = fq[i];
-            if((i==0 || i==4 || i==8 || i==14 || i==20)){
-                if(maxv<f) maxv = f;
+        int maxv = 0,maxc = 0;
+        for(int i=0;i<fr.length;i++){
+            if(i==0 || i==4 || i==8 || i==14 || i==20){
+                if(maxv<fr[i]) maxv = fr[i];
+            }else{
+                if(maxc<fr[i]) maxc = fr[i];
             }
-            else if(maxc < f) maxc = f;
         }
-        return maxv + maxc;
+        return maxc+maxv;
     }
 }
