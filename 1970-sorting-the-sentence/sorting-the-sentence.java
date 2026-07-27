46a35bc82339e10a9c91b1f73673fec61522ea78
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-27 11:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (28.13%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -7,10 +7,10 @@ class Solution {
             i = (int) (st.charAt(st.length()-1)-'0');
             res[i-1] = st.substring(0,st.length()-1);
         }
-        StringBuilder sb = new StringBuilder();
-        for(i=0;i<res.length-1;i++)
-            sb.append(res[i]+" ");
-        sb.append(res[i]);
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder();
+        // for(i=0;i<res.length-1;i++)
+        //     sb.append(res[i]+" ");
+        // sb.append(res[i]);
+        return String.join(" ",res);
     }
 }
