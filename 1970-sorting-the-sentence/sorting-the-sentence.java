aec563f8178300afab96582e79980694e988dcfd
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-27 11:54:44 +0530

    Time: 1 ms (39.08%) | Memory: 42.9 MB (46.80%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -2,19 +2,15 @@ class Solution {
     public String sortSentence(String s) {
         String[] str = s.split(" ");
         String[] res = new String[str.length];
+        int i=0;
         for(String st:str){
-            int i = (int) (st.charAt(st.length()-1)-'0');
+            i = (int) (st.charAt(st.length()-1)-'0');
             res[i-1] = st.substring(0,st.length()-1);
         }
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<res.length;i++){
-            if(i==res.length-1){
-                sb.append(res[i]);
-            }else{
-                sb.append(res[i]+" ");
-            }
-            
-        }
+        for(i=0;i<res.length-1;i++)
+            sb.append(res[i]+" ");
+        sb.append(res[i]);
         return sb.toString();
     }
 }
