df53639aec8261339e3a5f38bb4cd1845ca349d6
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-07-30 08:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (47.13%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,14 +1,15 @@
 class Solution {
     public int minimumPushes(String word) {
-        int n = word.length();
-        if(n<=8){
-            return n;
-        }else if(n <= 8*2){
-            return 8+(n-8)*2;
-        }else if(n <= 8*3){
-            return (n-8)*3;
-        }else{
-            return 16 + (n-16)*4;
-        }
+        //int n = word.length();
+        // if(n<=8){
+        //     return n;
+        // }else if(n <= 8*2){
+        //     return 8+(n-8)*2;
+        // }else if(n <= 8*3){
+        //     return (n-8)*3;
+        // }else{
+        //     return 16 + (n-16)*4;
+        // }
+        return 8 * (word.length()/8) * (word.length()/8 + 1)/2 + (word.length()/8 + 1) * (word.length()%8);
     }
 }
