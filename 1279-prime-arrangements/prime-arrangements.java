0a828a270fc46adc23ba96180f92937c7e75d9de
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-04 16:50:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (84.33%) - LeetSync

--- 1279-prime-arrangements/prime-arrangements.java
+++ 1279-prime-arrangements/prime-arrangements.java
@@ -1,29 +1,39 @@
 class Solution {
+     private static final int MOD = 1_000_000_007;
     public int numPrimeArrangements(int n) {
-        int MOD = 1_000_000_007;
-        boolean[] prime = new boolean[n+1];
-        Arrays.fill(prime,true);
-        prime[0] = prime[1] = false;
-        for(int i=2;i*i<=n;i++){
-            if(prime[i]){
-                for(int p=i*i;p<=n;p+=i){
-                    prime[p] = false;
-                }
+         int primes = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                primes++;
             }
         }
-        int count = 0;
-        for(int i=2;i<=n;i++){
-            if(prime[i]) count++;
+
+        int nonPrimes = n - primes;
+        long ans = 1;
+
+        for (int i = 2; i <= primes; i++) {
+            ans = ans * i % MOD;
         }
 
-        long ans = 1;
-        for(int i=1;i<=count;i++){
-            ans = (ans*i) % MOD;
-        }
-        for(int i=1;i<=n-count;i++){
-            ans = (ans*i) % MOD;
+        for (int i = 2; i <= nonPrimes; i++) {
+            ans = ans * i % MOD;
         }
 
         return (int) ans;
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
