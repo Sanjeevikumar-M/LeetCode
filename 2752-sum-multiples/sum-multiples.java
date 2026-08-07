8535981a21de3eb4184103aec3dea3ab70725903
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-07 10:46:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (90.86%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,12 +1,11 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        if(n==1 || n==2) return 0;
-        int sum = 0;
-        for(int i=3;i<=n;i++){
-            if(i%3==0) sum+=i;
-            else if(i%5==0) sum+=i;
-            else if(i%7==0) sum+=i;
-        }
-        return sum;
+        return sum(n, 3) + sum(n, 5) + sum(n, 7)
+                - sum(n, 15) - sum(n, 21) - sum(n, 35)
+                + sum(n, 105);
+    }
+    private int sum(int n, int k) {
+        int m = n / k;
+        return k * m * (m + 1) / 2;
     }
 }
