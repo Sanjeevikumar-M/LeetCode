d5ba7fc3f909fb477dfc5391dc469ab10127649e
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-12 09:04:19 +0530

    Time: 29 ms (99.67%) | Memory: 109.4 MB (9.62%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,22 +1,29 @@
 class Solution {
+    static class Counter {
+        int cnt = 0;
+    }
     public int maxSubarrayLength(int[] nums, int k) {
-        // Intuition: window size never shrinks, only slides; track how many values exceed k
-        int n = nums.length;
-        int left = 0;
-        int bad = 0;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int right = 0; right < n; right++) {
-            int c = nums[right];
-            int cnt = freq.getOrDefault(c, 0) + 1;
-            freq.put(c, cnt);
-            if (cnt == k + 1) bad++;
-            if (bad == 0) continue;
-            int d = nums[left];
-            int dc = freq.get(d) - 1;
-            freq.put(d, dc);
-            if (dc == k) bad--;
-            left++;
+        int N = nums.length;
+        Map<Integer, Counter> map = new HashMap<>();
+
+        int ll = 0;
+        int rr = 0;
+        int result = 0;
+        while (rr < N) {
+            int num = nums[rr++];
+            Counter counter = map.get(num);
+            if ( counter == null ) map.put(num, counter = new Counter());
+            if ( counter.cnt < k ) {
+                counter.cnt++;
+            } else {
+                int num2 = 0;
+                while ( (num2 = nums[ll++]) != num ) {
+                    Counter counter2 = map.get(num2);
+                    counter2.cnt--;
+                }
+            }
+            result = Math.max(result, rr - ll);
         }
-        return n - left;
+        return result;
     }
 }
