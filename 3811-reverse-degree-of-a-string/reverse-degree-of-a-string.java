cfb5b98302029581c4bd89acf672aa60fe2cd759
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-20 12:36:05 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (49.81%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public int reverseDegree(String s) {
         int sum = 0;
-        int[] arr = {26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+        //int[] arr = {26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
         for(int i=0;i<s.length();i++){
-            int ch = s.charAt(i) - 'a';
-            sum+= (arr[ch]*(i+1));
+            //int ch = s.charAt(i) - 'a';
+            sum+= (('z'-s.charAt(i)+1)*(i+1));
         }
         return sum;
     }
