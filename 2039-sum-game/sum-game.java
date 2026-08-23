78473152b98e8f400a82783461d58c55e4aa9a42
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-23 08:36:37 +0530

    Time: 5 ms (100.00%) | Memory: 46.8 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,13 +1,28 @@
 class Solution {
+
     public boolean sumGame(String num) {
-        int q = 0, d = 0, n = num.length();
-        for (int i = 0; i < n; i++){
-            if (num.charAt(i) == '?'){
-                q += 2* i < n? 1 : -1;
-            }else{
-                d += (2 * i < n? 1 : -1) * (num.charAt(i) - '0');
+        int n = num.length();
+        int[] left = get(num.substring(0, n / 2));
+        int[] right = get(num.substring(n / 2, n));
+
+        int n0 = left[0],
+            q0 = left[1];
+        int n1 = right[0],
+            q1 = right[1];
+
+        return (q0 + q1) % 2 == 1 || n0 - n1 != ((q1 - q0) * 9) / 2;
+    }
+
+    private int[] get(String s) {
+        int nn = 0,
+            qq = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '?') {
+                qq++;
+            } else {
+                nn += ch - '0';
             }
         }
-        return (q & 1) > 0 || q * 9 + 2 * d != 0;
+        return new int[] { nn, qq };
     }
 }
