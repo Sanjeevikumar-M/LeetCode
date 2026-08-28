46e43abd50f43f9144894840f50df6f6b450cffd
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-08-28 12:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 82.7 MB (23.37%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.java
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -1,30 +1,26 @@
 class Solution {
-    private static boolean isPrime(int num){
-        if(num==1) return false;
-        for(int i=2;i*i<=num;i++){
-            if(num%i == 0) return false;
+    private boolean isPrime(int n) {
+        if (n<=1) return false;
+        for (int i=2;i*i<=n;i++) {
+            if (n%i==0) return false;
         }
         return true;
     }
-
     public int diagonalPrime(int[][] nums) {
-        int max = 0;
-        int lrow = 0;
-        int lcol = 0;
-        int rcol = nums[0].length - 1;
-        while(lrow < nums.length){
-            boolean lprime = isPrime(nums[lrow][lcol]);
-            boolean rprime = isPrime(nums[lrow][rcol]);
-            if(lprime){
-                max = nums[lrow][lcol] > max ? nums[lrow][lcol] : max;
+        int maxPrime=0;
+        int n=nums.length;
+        
+        for (int i=0;i<n;i++) {
+            int val1=nums[i][i];
+            if (val1>maxPrime && isPrime(val1)) {
+                maxPrime=val1;
             }
-            if(rprime){
-                max = nums[lrow][rcol] > max ? nums[lrow][rcol] : max;
+            int val2=nums[i][n - 1 - i];
+            if (val2>maxPrime && isPrime(val2)) {
+                maxPrime=val2;
             }
-            lrow++;
-            lcol++;
-            rcol--;
         }
-        return max;
+        
+        return maxPrime;
     }
 }
