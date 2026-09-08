834853012398fb3f95a4ea15084ff1daeada3fc8
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-09-08 13:40:58 +0530

    Time: 1 ms (80.84%) | Memory: 46.8 MB (15.51%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] decompressRLElist(int[] nums) {
-        List<Integer> res = new ArrayList<>();
-        for(int i=0;i+1<nums.length;i+=2){
-            int freq = nums[i];
-            int val = nums[i+1];
-            for(int j=0;j<freq;j++){
-                res.add(val);
-            }
+        int size = 0;
+        for(int i = 0; i < nums.length; i += 2) {
+            size += nums[i];
         }
-        int[] ans = new int[res.size()];
-        for(int i=0;i<res.size();i++){
-            ans[i]=res.get(i);
+        int[] ans = new int[size];
+        int index = 0;
+        for(int i = 0; i < nums.length; i += 2) {
+            int freq = nums[i];
+            int value = nums[i + 1];
+            for(int j = 0; j < freq; j++) {
+                ans[index] = value;
+                index++;
+            }
         }
         return ans;
     }
