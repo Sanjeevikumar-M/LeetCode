9cd1b74441f777f1be73f4c225d779ac99e4c03a
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-09-10 18:04:58 +0530

    Time: 2 ms (91.11%) | Memory: 47.2 MB (65.94%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -1,31 +1,17 @@
 class Solution {
     public int minMoves(int[] nums) {
-        // int n = nums.length;
-        // int count = 0;
-        // int sum = 0;
-        // int min = Integer.MAX_VALUE;
-
-        // for(int i:nums){
-        //     sum+=i;
-        //     min = i<min ? i : min;
-        // }
-
-        // int leftside = n*(min+count);
-        // int rightside = sum+count*(n-1);
-        // while(leftside!=rightside){
-        //     count++;
-        //     leftside = n*(min+count);
-        //     rightside = sum+count*(n-1);
-        // }
-        // return count;
-
-        if(nums==null || nums.length<=1) return 0;
-        long min = (long) nums[0];
-        long sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum+=(long) nums[i];
-            min = Math.min(min,nums[i]);
+        int n = nums.length;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
         }
-        return (int) (sum-min*nums.length);
+        int moves = 0;
+        for (int num : nums) {
+            moves += num - min;
+        }
+        return moves;
+
     }
 }
