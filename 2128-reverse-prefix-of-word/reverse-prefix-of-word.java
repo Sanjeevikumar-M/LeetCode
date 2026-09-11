d2e41127ef672b5ec92ad01bee2c29c8a702ebf7
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-09-11 11:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (98.96%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -1,35 +1,18 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int left = 0;
-        int right = 0;
-        int n = word.length();
-        boolean isFound = false;
-        while(right<n){
-            if(word.charAt(right) == ch){
-                right++;
-                isFound = true;
-                break;
-            }
-            right++;
-        }
-        
-        if(isFound){
-            String ss = word.substring(left,right);
-            String remss = word.substring(right,n);
-            char[] cha = ss.toCharArray();
-            right--;
-            while(left<right){
-                char c = cha[left];
-                cha[left] = cha[right];
-                cha[right] = c;
-                left++;
-                right--;
-            }
-            String revss = new String(cha);
-            String res = revss + remss;
-            return res;
-        }else{
+        int r = word.indexOf(ch);
+        if(r==-1){
             return word;
         }
+        int l = 0;
+        char[] c = word.toCharArray();
+        while(l<r){
+            char t = c[l];
+            c[l] = c[r];
+            c[r] = t;
+            l++;
+            r--;
+        } 
+        return new String(c);
     }
 }
