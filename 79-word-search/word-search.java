dba350636ca1a140b87898ce2b294ebcdece82af
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-09-22 14:50:32 +0530

    Time: 149 ms (36.72%) | Memory: 43.1 MB (43.73%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -8,8 +8,8 @@ class Solution {
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j] == word.charAt(0)){
-                    result = backtrack(board,word,visited,i,j,0);
+                if(board[i][j]==word.charAt(0)){
+                    result = backtrack(board,visited,word,m,n,i,j,0);
                     if(result) return true;
                 }
             }
@@ -17,21 +17,22 @@ class Solution {
         return false;
     }
 
-    private boolean backtrack(char[][] board,String word, boolean[][] visited,int i,int j,int index){
-        if(index == word.length()){
+    private static boolean backtrack(char[][] board, boolean[][] visited, String word, int m, int n, int i, int j, int index){
+        if(index >= word.length()){
             return true;
         }
 
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || visited[i][j] || board[i][j] != word.charAt(index)){
+        if(i<0 || i>=m || j<0 || j>=n || visited[i][j] || board[i][j] != word.charAt(index)){
             return false;
         }
 
         visited[i][j] = true;
 
-        if(backtrack(board,word,visited,i+1,j,index+1) ||
-           backtrack(board,word,visited,i-1,j,index+1) ||
-           backtrack(board,word,visited,i,j+1,index+1) ||
-           backtrack(board,word,visited,i,j-1,index+1)){
+        if(backtrack(board,visited,word,m,n,i+1,j,index+1) ||
+           backtrack(board,visited,word,m,n,i-1,j,index+1) ||
+           backtrack(board,visited,word,m,n,i,j-1,index+1) ||
+           backtrack(board,visited,word,m,n,i,j+1,index+1) 
+        ){
             return true;
         }
 
