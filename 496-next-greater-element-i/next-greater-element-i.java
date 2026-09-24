3b620202ce216b86f7eb10f48bf9a1aa44596641
Sanjeevikumar M <152852720+Sanjeevikumar-M@users.noreply.github.com>
2026-09-24 19:04:35 +0530

    Time: 3 ms (93.11%) | Memory: 44.6 MB (95.05%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int m = nums2.length;
-        int l = nums1.length;
-        int[] ans = new int[l];
-        for(int i=0;i<l;i++){
+        int m = nums1.length;
+        int l = nums2.length;
+        int[] ans = new int[m];
+        for(int i=0;i<m;i++){
             int n = -1;
             int st = 0;
-            for(int j=0;j<m;j++){
+            for(int j=0;j<l;j++){
                 if(nums2[j]==nums1[i]){
                     n = nums2[j];
                     st = j;
@@ -14,17 +14,19 @@ class Solution {
                 }
             }
             if(n==-1){
-                ans[i] = -1;
+                ans[i] = n;
                 continue;
             }
-            while(st<m){ 
+            while(st<l){
                 if(nums2[st]>n){
-                    ans[i] = nums2[st];
+                    ans[i]=nums2[st];
                     break;
                 }
                 st++;
             }
-            ans[i] = st==m ? -1 : nums2[st];
+            if(st==l){
+                ans[i] = -1;
+            }
         }
         return ans;
     }
